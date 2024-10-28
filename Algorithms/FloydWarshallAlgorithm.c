1b855a08013432e7094950b3b8527679907305ad
ohmschrodinger <omdhamame@gmail.com>
2024-10-29 00:28:56 +0530

    Floyd Warshall Algorithm

---
+++ Algorithms/FloydWarshallAlgorithm.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+#define INF 99999
+#define V 4  // Number of vertices in the graph
+
+// Function to implement Floyd-Warshall algorithm
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+
+    // Initialize the solution matrix same as input graph matrix
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    // Update the distance matrix
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    // Print the shortest distance matrix
+    printf("Shortest distances between every pair of vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 3, INF, 7},
+        {8, 0, 2, INF},
+        {5, INF, 0, 1},
+        {2, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
