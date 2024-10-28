ac470a84490a7282d26f89d511a843089bf9555e
Ajaydeep <71118663+ajydeep@users.noreply.github.com>
2024-10-28 20:54:47 +0530

    Create hangman_function.h

---
+++ Hangman Game/hangman_function.h
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+//declare fun
+void greet();
+void display_misses( int misses );
+void display_status( vector<char> incorrect, string answer);
+void end_game(string answer, string codeword);
